66a5ec9de9d3bfcfdee8cbd3c72ab082fd25360e
pankaj <pankajsemwalpankaj123@gmail.com>
2023-04-25 17:30:19 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,18 +1,19 @@
-const { City }= require('../models/index')
+const { City }= require('../models/index')   // models/index.js have all the models ,we can use it by destructuring
 
 class CityRepository{
 
-    async createCity({name}){
-
+    async createCity({ name }){    
         try{
-        
-            const city= await City.create({name});
-            return city;
 
+            const city= await City.create({
+                name : name     // model property : value
+            });
+            return city;
         }
         catch(error)
         {
-                throw { error };
+                console.log("something went wrong in the repository layer");
+                throw {error};
         }
     }
 
@@ -24,12 +25,40 @@ class CityRepository{
                             id:cityid
                         }
                       })      
-
+                
             }
             catch(error)
             {
                 throw {error};
             }
+    }
+    async getCity(cityid){
+
+        try{
+            const city= await City.findByPk(cityid);
+            return city;
+        }
+        catch(error)
+        {
+                throw {error};
+        }
+
+
+    }
+    async updateCity(cityid,data){ //data is a object containing what needs to be updated
+ 
+        try{
+            const city= await City.update(data,{
+                where:{
+                    id:cityid
+                }
+            })
+
+        }
+        catch(error){
+            throw {error}
+        }
+
     }
 
 }
