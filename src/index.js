cd69be845ab97d373246f88ca35f3b53fb4f1edb
pankaj <pankajsemwalpankaj123@gmail.com>
2023-04-26 22:09:23 +0530

    added airport seeders

--- src/index.js
+++ src/index.js
@@ -3,17 +3,23 @@ const {PORT} =require('./config/serverConfig');
 const bodyParser = require('body-parser');
 
 const ApiRoutes= require('./routes/index')
-
+const db= require('./models/index')
+const {City,Airport} =require('./models/index')
 const setupAndStartServer= async()=>{
     const app=express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
     app.use('/api',ApiRoutes)
-    
+
     app.listen(PORT,async ()=>{
-
+ 
         console.log(`server started at ${PORT}`)
-
+        if(process.env.SYNC_DB){
+          db.sequelize.sync({alter:true});
+          //synchorization is to be done whenever we add a new association 
+          // to use some functions like city.getAirports()
+        }
+      
     })
 }
 
